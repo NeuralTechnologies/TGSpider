35f4bf657cee22e2f091fc1935a8f41426fc1575
NeuralTechnologies <166009551+NeuralTechnologies@users.noreply.github.com>
2024-04-09 18:01:12 +0400

    Create update2.sql

---
+++ postgres/update2.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.tg_accounts(
+	id, api_id, api_hash, phone, session_name, password, is_initialized, date_start_used, date_end_used)
+	OVERRIDING SYSTEM VALUE
+	VALUES ('3','23900952', '3560c2b36d325c3b2e16058d1508c52a', '+447407737160', '447407737160', '0323', false, '2024-04-01', '2024-04-01');
+	
+INSERT INTO public.tg_accounts(
+	id, api_id, api_hash, phone, session_name, password, is_initialized, date_start_used, date_end_used)
+	OVERRIDING SYSTEM VALUE
+	VALUES ('4', '27828954', '1b6d488b7e69e90094609605e7c6ca5d', '+447444684717', '447444684717', '3454', false, '2024-04-01', '2024-04-01');
+	
